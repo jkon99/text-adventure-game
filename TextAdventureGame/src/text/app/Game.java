8e5e961231a8793a78df962258f223d0cb137f39
Jon <Jon@Jon-PC>
2023-09-24 00:22:05 -0400

    initial commit

---
+++ TextAdventureGame/src/text/app/Game.java
@@ -0,0 +1,8 @@
+package text.app;
+import java.util.*;
+
+public class Game {
+	//create necessary variables and structures
+	//set up starting room and scenario, display text
+	//take input
+}
---
+++ TextAdventureGame/src/text/app/Main.java
@@ -0,0 +1,12 @@
+package text.app;
+
+public class Main {
+
+	//main method
+	//utilize Game.java
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
